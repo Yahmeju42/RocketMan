bbf101464b9be9ec9d93eaa8332d2aa2752e9cae
tvalour <57670501+tvalour@users.noreply.github.com>
2020-05-13 17:29:12 +0200

    Update game.py

--- game.py
+++ game.py
@@ -14,16 +14,32 @@ class Game():
         pygame.display.set_caption(name) #défini le nom de la fenêtre
         icon = pygame.image.load("textures/icon.png").convert() #charge l'icon de la fenêtre
         pygame.display.set_icon(icon) #défini l'icon de la fenêtre
-
+        self.vaisseauposition = [0,0]
+        self.vitesse = [0,0]
+        self.acceleration = [0,0]
 
     #def loadmap(self):    Charge la map du jeu
     def events(self):
+        self.action = "z"
         #Capte les évènements claviers
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 #quitte le jeu
                 self.play = False
-    #def calc(self):       Calcule les positions et les colisions
+    def calc(self):
+    # Calcule les positions et les colisions
+
+
+        if self.action = "z" :
+            self.vitesse[0]+=1
+
+        self.vaisseauposition[0] += self.vitesse[0]
+
+
+
+
+
+
     #def write(self):      Affiche à l'écran les différents objets ayant changer de positions
     ##def win(self):        Le joueur a gagné
     ##def lose(self):       Le joueur a perdu
