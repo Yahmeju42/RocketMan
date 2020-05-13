8631f5dc4897cddc15dcf56d9d132027e7d578bf
Yahmeju42 <61884872+Yahmeju42@users.noreply.github.com>
2020-05-13 18:13:27 +0200

    plein de truc

--- game.py
+++ game.py
@@ -18,38 +18,52 @@ class Game():
         self.vitesse = [0,0]
         self.acceleration = [0,0]
 
-    #def loadmap(self):    Charge la map du jeu
+    def loadmap(self):
+        #Charge la map du jeu
+        print("loading map")
+        self.fond = pygame.image.load("textures/fond.png").convert()
+        self.vaisseautexture = pygame.image.load("textures/fusée.png").convert_alpha()
+        self.vaisseautexture = pygame.transform.scale(self.vaisseautexture, (40,40))
     def events(self):
         self.action = "r"
         #Capte les évènements claviers
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                #quitte le jeu
-                self.play = False
-            if event.type == pygame.K_z:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_z:
                 #la touche avancer a été appuyer
                 self.action = "z"
+                self.play = True
+                print("Z")
+            if event.type == pygame.QUIT:
+                #quitte le jeu
+                self.play = False
+                print("Aurevoir")
+
     def calc(self):
     # Calcule les positions et les colisions
 
 
-        if self.action = "z" :
+        if self.action == "z" :
             self.vitesse[0]+=1
 
         self.vaisseauposition[0] += self.vitesse[0]
 
+    def write(self):
+        #Affiche à l'écran les différents objets ayant changer de positions
+        self.window.blit(self.fond, (0,0))
+        self.window.blit(self.vaisseautexture, (self.vaisseauposition[0], self.vaisseauposition[1]))
 
 
-
-
-
-    #def write(self):      Affiche à l'écran les différents objets ayant changer de positions
+        pygame.display.flip()
     ##def win(self):        Le joueur a gagné
     ##def lose(self):       Le joueur a perdu
 
     def loop(self):
+
         #La boucle de jeu qui lance events, calc et write en boucle et qui se charge de la synchronisation
+        self.loadmap()
         print("lancement de la boucle du jeu")
         while(self.play == True):
-            print("jeu")
+            #print("jeu")
             self.events()
+            self.calc()
+            self.write()
